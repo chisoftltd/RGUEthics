23b5ba4ce8c96bc2e6531ec0b990b30100d250df
chisoftltd <chisoftltd@gmail.com>
2017-04-18 18:39:58 +0100

    6rd commit

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
         <ul class="footer-links">
             <li><a href="index.php">Home</a></li>
             <li><a href="disclamer.php">Disclaimer</a></li>
-            <li><a href="index.html">Freedom of information</a></li>
+            <li><a href="freedom-of-information.php">Freedom of information</a></li>
             <li><a href="index.html">Privacy and cookie statement</a></li>
             <li><a href="index.html">Complaints</a></li>
             <li><a href="index.html">Copyright ©2017</a></li>
