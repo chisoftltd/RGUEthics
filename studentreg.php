c53ec93cc434f6ce06dbd13c3aa7bbb91810ea5b
chisoftltd <chisoftltd@gmail.com>
2017-04-19 19:06:40 +0100

    20th commit

--- studentreg.php
+++ studentreg.php
@@ -129,9 +129,11 @@ function test_input($data)
         ?>
         <div class="form-group">
             <div class="alert alert-<?php echo ($errTyp == "success") ? "success" : $errTyp; ?>">
-                <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG;
-echo "<br>";
+                <span class="glyphicon glyphicon-info-sign"></span>
+                <?php
                 echo $res;
+                echo "<br>";
+                echo $errMSG;
                 ?>
 
             </div>
