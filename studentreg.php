b0d9248aac6a76555eee7fe44eb1e280fb5a8319
chisoftltd <chisoftltd@gmail.com>
2017-04-19 18:04:07 +0100

    18th commit

--- studentreg.php
+++ studentreg.php
@@ -111,12 +111,9 @@ function test_input($data)
         </div>
 
         <div class="form-group">
-            <label class="col-xs-3 control-label">Date</label>
-            <div class="col-xs-5 date">
-                <div class="input-group input-append date" id="datePicker">
-                    <input type="text" class="form-control" name="date" />
-                    <span class="input-group-addon add-on"><span class="glyphicon glyphicon-calendar"></span></span>
-                </div>
+            <div class="input-group input-append date" id="datePicker">
+                <label class="col-xs-3 control-label">Date</label>
+                <input type="text" class="form-control" name="date"/>
             </div>
         </div>
 
