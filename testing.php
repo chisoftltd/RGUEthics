119c5022f828a850aa0c3f63f489ea76487de2d6
chisoftltd <chisoftltd@gmail.com>
2017-04-19 20:35:50 +0100

    20th commit

--- testing.php
+++ testing.php
@@ -18,9 +18,9 @@ if (!$link) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>RGUEthics - Experiment Approval Officers (EAO)</title>
-    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css"/>
     <link rel="stylesheet" href="css/main-style.css">
 </head>
 <body>
@@ -32,29 +32,29 @@ if (!$link) {
     <?php
     /* connect to the db */
 
-    mysqli_select_db($link,"localdb");
+    /*mysqli_select_db($link,"localdb");
 
-    /* show tables */
+     show tables
     $result = mysqli_query($link,"Show tables") or die('cannot show tables');
     while($tableName = mysqli_fetch_row($result)) {
 
         $table = $tableName[0];
-
-        echo '<h3>',$table,'</h3>';
-        $result2 = mysqli_query($link, 'select Number, Name, supervisor, ProjectTopic, StartDate, Endate from'.$table) or die('cannot show columns from '.$table);
-        if(mysqli_num_rows($result2)) {
-            echo '<table cellpadding="0" cellspacing="0" class="table table-striped">';
-            echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
-            while($row2 = mysqli_fetch_row($result2)) {
-                echo '<tr>';
-                foreach($row2 as $key=>$value) {
-                    echo '<td>',$value,'</td>';
-                }
-                echo '</tr>';
+*/
+    echo '<h3>', $table, '</h3>';
+    $result2 = mysqli_query($link, "SELECT Number, Name, supervisor, ProjectTopic, StartDate, Endate FROM projethics") or die('cannot show columns from ' . $table);
+    if (mysqli_num_rows($result2)) {
+        echo '<table cellpadding="0" cellspacing="0" class="table table-striped">';
+        echo '<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default<th>Extra</th></tr>';
+        while ($row2 = mysqli_fetch_row($result2)) {
+            echo '<tr>';
+            foreach ($row2 as $key => $value) {
+                echo '<td>', $value, '</td>';
             }
-            echo '</table><br />';
+            echo '</tr>';
         }
+        echo '</table><br />';
     }
+    //}
     ?>
 </div>
 <div>
