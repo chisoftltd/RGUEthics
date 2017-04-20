c2addc0437760c330ccaea6755d029e0fb5c1404
chisoftltd <chisoftltd@gmail.com>
2017-04-21 00:13:50 +0100

    30th commit

--- register.php
+++ register.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = test_input($_POST["id"]);
     $name = test_input($_POST["name"]);
     $email = test_input($_POST["email"]);
-    $pass = test_input($_POST["pass"]);
+    $password = test_input($_POST["pass"]);
 
     // Check connection
     if ($link->connect_error) {
@@ -48,10 +48,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
     // password validation
-    if (empty($pass)) {
+    if (empty($password)) {
         $error = true;
         $passError = "Please enter password.";
-    } else if (strlen($pass) < 6) {
+    } else if (strlen($password) < 6) {
         $error = true;
         $passError = "Password must have atleast 6 characters.";
     }
@@ -59,17 +59,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($id)) {
         $error = true;
         $idError = "Please enter ID Number.";
-    } else if (strlen($id) < 0 || strlen($id) > 7) {
+    } else if (strlen($id) < 0 || strlen($id) > 11) {
         $error = true;
-        $idError = "ID must have atleast 7 digits.";
+        $idError = "ID must have atleast 11 digits.";
     }
     // password encrypt using SHA256();
-    $password = hash('sha256', $pass);
+    $passwd = hash('sha256', $password);
 
     // if there's no error, continue to signup
     if (!$error) {
 
-        $query = "INSERT INTO users(userId, userName,userEmail,userPass) VALUES('$id', '$name','$email','$pass')";
+        $query = "INSERT INTO users VALUES('$id', '$name','$email','$passwd')";
         $res = mysqli_query($link, $query);
 
 
@@ -78,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $errMSG = "Successfully registered, you may login now";
             unset($name);
             unset($email);
-            unset($pass);
+            unset($passwd);
         } else {
             $errTyp = "danger";
             $errMSG = "Something went wrong, try again later...";
@@ -86,7 +86,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     // if session is not set this will redirect to login page
     if (!isset($_SESSION['user'])) {
-        header("Location: studentreg.php");
+        header("Location: loginhome.php");
         exit;
     }
 
@@ -98,124 +98,137 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $data = htmlspecialchars($data);
         return $data;
     }
-}
+}/*
 if (!mysqli_query($link, "SET a=1")) {
     printf("Errormessage: %s\n", mysqli_error($link));
 }
-//echo error_reporting(E_ALL);
+echo error_reporting(E_ALL);
+*/
 ?>
-<!DOCTYPE html>
-<html>
+    <!DOCTYPE html>
+    <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <title>RGUEthics - Registration System</title>
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-        <link rel="stylesheet" href="style.css" type="text/css" />
+        <link rel="stylesheet" href="style.css" type="text/css"/>
         <link rel="stylesheet" href="css/main-style.css">
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+              integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+              crossorigin="anonymous">
         <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+              integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+              crossorigin="anonymous">
         <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+                crossorigin="anonymous"></script>
+        <script>
+            function newDoc() {
+                window.location.assign("login.php")
+            }
+        </script>
     </head>
     <body>
-        <div>
-            <?php include 'include/loginheader.php'; ?>
-        </div>
-        <div>
-            <hr>
-        </div>
-        <div class="container">
+    <div>
+        <?php include 'include/loginheader.php'; ?>
+    </div>
+    <div>
+        <hr>
+    </div>
+    <div class="container">
 
-            <div id="login-form">
-                <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="on">
+        <div id="login-form">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="on">
 
-                    <div class="col-md-12">
+                <div class="col-md-12">
 
-                        <div class="form-group">
-                            <h2 class="">Sign Up.</h2>
-                        </div>
-
-                        <div class="form-group">
-                            <hr />
-                        </div>
-
-                        <?php
-                        if (isset($errMSG)) {
-                            ?>
-                            <div class="form-group">
-                                <div class="alert alert-<?php echo ($errTyp == "success") ? "success" : $errTyp; ?>">
-                                    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
-                                </div>
-                            </div>
-                            <?php
-                        }
-                        ?>
-
-                        <div class="form-group">
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                                <input type="number" name="id" class="form-control" placeholder="Enter ID Number" maxlength="50" value="<?php echo $name ?>" />
-                            </div>
-                            <span class="text-danger"><?php echo $idError; ?></span>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                                <input type="text" name="name" class="form-control" placeholder="Enter Name" maxlength="50" value="<?php echo $name ?>" />
-                            </div>
-                            <span class="text-danger"><?php echo $nameError; ?></span>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-                                <input type="email" name="email" class="form-control" placeholder="Enter Your Email" maxlength="40" value="<?php echo $email ?>" />
-                            </div>
-                            <span class="text-danger"><?php echo $emailError; ?></span>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="input-group">
-                                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                                <input type="password" name="pass" class="form-control" placeholder="Enter Password" maxlength="15" />
-                            </div>
-                            <span class="text-danger"><?php echo $passError; ?></span>
-                        </div>
-
-                        <div class="form-group">
-                            <hr />
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-block btn-primary">Sign Up</button>
-                        </div>
-
-                        <div class="form-group">
-                            <hr />
-                        </div>
-
-                        <div class="form-group">
-                            <a href="loginhome.php">Sign in Here...</a>
-                        </div>
-
+                    <div class="form-group">
+                        <h2 class="">Sign Up.</h2>
                     </div>
 
-                </form>
-            </div>
+                    <div class="form-group">
+                        <hr/>
+                    </div>
 
-        </div>
-        <div>
-            <hr>
-        </div>
-        <div>
-            <?php include 'include/footer.php'; ?>
+                    <?php
+                    if (isset($errMSG)) {
+                        ?>
+                        <div class="form-group">
+                            <div class="alert alert-<?php echo ($errTyp == "success") ? "success" : $errTyp; ?>">
+                                <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                    ?>
 
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                            <input type="number" name="id" class="form-control" placeholder="Enter Student Number"
+                                   maxlength="50" value="<?php echo $id ?>"/>
+                        </div>
+                        <span class="text-danger"><?php echo $idError; ?></span>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                            <input type="text" name="name" class="form-control" placeholder="Enter Student Name"
+                                   maxlength="50" value="<?php echo $name ?>"/>
+                        </div>
+                        <span class="text-danger"><?php echo $nameError; ?></span>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+                            <input type="email" name="email" class="form-control" placeholder="Enter Your Email"
+                                   maxlength="40" value="<?php echo $email ?>"/>
+                        </div>
+                        <span class="text-danger"><?php echo $emailError; ?></span>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+                            <input type="password" name="pass" class="form-control" placeholder="Enter Password"
+                                   maxlength="20" value="<?php echo $passwd ?>"/>
+                        </div>
+                        <span class="text-danger"><?php echo $passError; ?></span>
+                    </div>
+
+                    <div class="form-group">
+                        <hr/>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-block btn-primary" onclick="newDoc()">Sign Up</button>
+                    </div>
+
+                    <div class="form-group">
+                        <hr/>
+                    </div>
+
+                    <div class="form-group">
+                        <a href="loginhome.php">Sign in Here...</a>
+                    </div>
+
+                </div>
+
+            </form>
         </div>
+
+    </div>
+    <div>
+        <hr>
+    </div>
+    <div>
+        <?php include 'include/footer.php'; ?>
+
+    </div>
     </body>
-</html>
+    </html>
 <?php ob_end_flush(); ?>
