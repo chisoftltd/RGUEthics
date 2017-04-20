faa33d898e7f2d499e812fca4cc89cd4ed9f8e52
chisoftltd <chisoftltd@gmail.com>
2017-04-20 15:51:14 +0100

    28th commit

--- EAO.php
+++ EAO.php
@@ -48,7 +48,7 @@ if (!$link) {
     if (mysqli_num_rows($result2)) {
         echo '<table cellpadding="0" cellspacing="0" class="table table-striped">';
         echo '<tr><th>Number</th><th>Name</th><th>Supervisor</th><th>Project Topic</th><th>Start Date<th>End Date</th></tr>';
-        while ($row2 = mysqli_fetch_row($result2)) {
+        while ($row2 = mysqli_fetch_array($result2)) {
             echo '<tr>';
             echo "<td><a href='' onclick='projpages()'>" . $row2[Number] . "</a></td>";
             echo "<td>" . $row2[Name] . "</td>";
